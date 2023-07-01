7b7adeaef4559bd8a0e18ee08472e3187b7e22bc
Vie <the_ruesi@zoho.com>
2023-07-01 18:31:03 +0100

    widget code

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 // go out to local storage and retrieve any todos: 
 // let todos = JSON.parse(localStorage.getItem("todos")) || [];
-
 const todos = JSON.parse(localStorage.getItem("todos"));
 
 console.log(todos)
-
-
-
 
 // Run this on start up and any time crud changes occur on todos:
 const refreshApp = () => {
@@ -14,16 +10,39 @@ const refreshApp = () => {
   // Make a widget for each object in the todos array eg: priority, mifCheck etc.
   let html = '';
 
+  // Function to build a todos widget
   const widgetBuilder = () => {
-    todos.forEach(widget => {
+    console.log('hello')
+    for(let keys in todos) {
       html += `
-        
+        <section class="widget class=${keys}">
+          <h2>${keys}</h2>
+        </section>
       `
-    });
+      html += `<ul class="sortable-list ${keys}">`
+      if(todos[keys].lengh === 0) {
+        html += "Nothing todo here!"
+      }else {
+        todos[keys].forEach(key => {
+          html += `
+           <p> ${key.description} </p>
+          `
+        })
+      }
+
+      html += `</ul>`
+
+    }
+
+    document.querySelector("main").innerHTML = html
 
   }
 
+  widgetBuilder();
+
 }
+
+refreshApp();
 
 
 
