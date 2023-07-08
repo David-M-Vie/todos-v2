3d85bccc9728b83fdb706bf1188d4971e5dccbab
Vie <the_ruesi@zoho.com>
2023-07-08 23:26:46 +0100

    columns complete

--- app.js
+++ app.js
@@ -4,152 +4,130 @@ const todos = JSON.parse(localStorage.getItem("todos"));
 
 console.log(todos)
 
-// Run this on start up and any time crud changes occur on todos:
-const refreshApp = () => {
-
-  // Make a widget for each object in the todos array eg: priority, mifCheck etc.
+const widgetHtml = (keys) => {
   let html = '';
 
-  // Function to build a todos widget
-  const widgetBuilder = () => {
-
-    for(let keys in todos) {
+  html += `
+    <section class="widget ${keys}">
+      <h2>${keys}</h2>
+      <ul class="sortable-list ${keys}">
+  `
+  if(todos[keys].lengh === 0) {
+    html += "Nothing todo here!"
+  }else {
+    todos[keys].forEach(key => {
       html += `
-        <section class="widget ${keys}">
-          <h2>${keys}</h2>
-          <ul class="sortable-list ${keys}">
+        <li class="item ${key.completed ? "completed" : ""}" 
+        draggable="true"
+        data-uid ="{key.uid}
+        "> 
+          <h4 class="top-row">
+            <span 
+              contentEditable="true"
+              onblur=""
+            >
+              Id: ${key.id}
+            </span>
+            <input 
+              type="checkbox"
+              ${key.completed ? "checked" : ""}
+            />
+            <button
+              class="btn2"
+              onclick=""
+            >
+              Delete
+            </button>
+          </h4>
+          <div class="bottom-row">
+            <div class="col-1">
+              <p
+                contentEditable="true"
+                onblur=""
+                class="text"
+              >
+                ${key.description}
+              </p>
+            </div>
+            <div class="col-2">
+              <p class="text">
+                Due: ${key.dueDate}
+              </p>
+            </div>
+          </div>
+        </li>          
       `
-      if(todos[keys].lengh === 0) {
-        html += "Nothing todo here!"
-      }else {
-        todos[keys].forEach(key => {
-          html += `
-           <li class="item ${key.completed ? "completed" : ""}" 
-            draggable="true"
-            data-uid ="{key.uid}
-           "> 
-              <h4 class="top-row">
-                <span 
-                  contentEditable="true"
-                  onblur=""
-                >
-                  Id: ${key.id}
-                </span>
-                <input 
-                  type="checkbox"
-                  ${key.completed ? "checked" : ""}
-                />
-                <button
-                  class="btn2"
-                  onclick=""
-                >
-                  Delete
-                </button>
-              </h4>
-              <div class="bottom-row">
-                <div class="col-1">
-                  <p
-                    contentEditable="true"
-                    onblur=""
-                    class="text"
-                  >
-                    ${key.description}
-                  </p>
-                </div>
-                <div class="col-2">
-                  <p class="text">
-                    Due: ${key.dueDate}
-                  </p>
-                </div>
-              </div>
-           </li>          
-          `
-        })
-      }      
-      html += `</ul></section> `
-    }
-
-    document.querySelector("main").innerHTML = html;
-
-    const sortableLists = document.querySelectorAll(".sortable-list"); // array
-    console.log("SortableLists: ", sortableLists)
-    sortableLists.forEach(list => {
-      const items = list.querySelectorAll(".item");
-      items.forEach(item => {
-        //console.log(item)
-        item.addEventListener("dragstart", () => {
-          setTimeout(() => item.classList.add("dragging"), 0)
-        })
-        // Removing dragging class from item on dragend event
-        item.addEventListener("dragend", () => {
-          item.classList.remove("dragging")
-          //when drag ends, need to update teh todos list with the new order
-         // setNewOrder(items.dataset.uid); // WORK IN PROGRESS
-        })
-        item.querySelector('input').addEventListener("change", (e) => {
-          toggleCompleted(e)
-        })
-      })
     })
+  }      
+  html += `</ul></section> `
+  return html;
+}
 
-    const initSortableList = (e) => {
-      e.preventDefault();
-      const draggingItem = document.querySelector(".dragging");
-      // find the UL parent of the item being dragged
-      console.log('draggingItem', draggingItem)
-      const sortableList = draggingItem.closest("ul");
-      console.log("sortableList", sortableList);
-      let siblings = [...sortableList.querySelectorAll(".item:not(.dragging)")]
-      let nextSibling = siblings.find(sibling => {
-        return e.clientY <= sibling.offsetTop + sibling.offsetHeight / 2
-      })
-      sortableList.insertBefore(draggingItem, nextSibling);
+const refreshApp = () => {
+  // make a widget for each object in the todos array: eg, priority, mifCheck etc. 
+
+  // function to build widgets. 
+  const widgetBuilder = () => {
+    let html = '';
+    for(let keys in todos) {
+      console.log('keys ', keys)
+      if(keys === 'priority') { // If it's the main todo widget..
+        document.querySelector('#priority-section').innerHTML = widgetHtml(keys)
+      }else { // it must be secondary widgets
+        html += widgetHtml(keys)
+        document.querySelector('#other-sections').innerHTML = html;
+      }
     }
-
-    sortableLists.forEach(list => {
-      list.addEventListener("dragover", initSortableList);
-      list.addEventListener("dragenter", e => e.preventDefault());
-    })
-  }
+    
+  } // end of widgetBuilder function
 
   widgetBuilder();
 
 
+  const sortableLists = document.querySelectorAll(".sortable-list");
+  console.log('sortableLists ', sortableLists);
+
+  sortableLists.forEach(list => {
+    const items = list.querySelectorAll('.item');
+    items.forEach(item => {
+      console.log('list item ', item)
+      item.addEventListener("dragstart", () => {
+        setTimeout(() => item.classList.add("dragging"), 0)
+      })
+
+      // Removing dragging class from item on dragend event
+      item.addEventListener("dragend", () => {
+        item.classList.remove("dragging")
+        // When drag ends, need to update the todos list with the new order
+        // setNewOrder(items.dataset.uid) // WORK IN PROGRESS
+      })
+      item.querySelector('input').addEventListener("change", (e) => {
+        toggleCompleted(e)
+      })
+
+      
+    })
+  })
+
+  const initSortableList = (e) => {
+    e.preventDefault();
+    const draggingItem = document.querySelector(".dragging");
+    // find the UL parent of the item being dragged
+    const targetList = draggingItem.closest('ul');
+    console.log("targetList", targetList)
+  }
 
 
-
-}
-
-/* ==================
- ===== FUNCTIONS ====
-=================== */
-
-
-/* ============
-    Add a todo
-=============== */
-const addTodo = () => {
-  return;  // work in progress
-}
-
-
-/* ===============================
-  Toggle a todos completed status
-================================== */
-const toggleCompleted = (e) => {
-   return; // work in progress...
-}
-
-/* ==============================
-  When order of todos is changed
-================================= */
-const setNewOrder = (uid) => {
-  return;  // work in progress...
-}
-
-
+} // close refreshApp function
 
 refreshApp();
 
 
+/////////////////
+/// FUNCTIONS ///
+/////////////////
+
+const toggleCompleted = (e) => {
+  return; // WORK IN PROGRESS
+}
 
