08a7878565afda39b09f30c5c95b7289687a02e8
Vie <the_ruesi@zoho.com>
2023-08-03 21:24:06 +0100

    laoyut style

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 // go out to local storage and retrieve any todos: 
-const todos = JSON.parse(localStorage.getItem("todos")) || {
-  "priority": []
-};
+// const todos = JSON.parse(localStorage.getItem("todos")) || {
+//   "priority": []
+// };
 
-
+let todos = {
+  priority: [{id: 1, description: "Kick the Cat", dueDate:"tbc", completed: false},{id: 2, description: "Walk the Dog", dueDate:"tbc", completed: false}],
+  mifCheck: [{id: 232, description: "Check Bob's MIF applied", dueDate: "tbc", completed: false}, {id: 71232, description: "Check Frank's MIF applied", dueDate: "tbc", completed: false}],
+  beadyEye: [{id: 9123, description: "Perhaps Fraud", dueDate:"tbc", completed: false},{id: 12392, description: "Look into this...", dueDate:"tbc", completed: false}, {id: 11, description: "Check these out...", dueDate:"tbc", completed: false}]
+}
 
 console.log(todos)
 
